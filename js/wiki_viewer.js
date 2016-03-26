589e8de984342a2bca5374aca43f42dafeceb2e0
Emil Pausz <emilpausz@gmail.com>
2016-03-26 09:47:31 +0100

    add basic functionality for wiki search

--- js/wiki_viewer.js
+++ js/wiki_viewer.js
@@ -3,9 +3,10 @@ $(function () {
 		// variables holds DOM elements
 		var $searchContainer,
 			$searchInput;
-
+		
 		// modue for handled events
 		var WikiViewerEvents = (function(){
+			var wikiQueryOffset = 0;
 
 			// resize text input
 			function resizeUpSearchInput(ev) {
@@ -15,7 +16,6 @@ $(function () {
 					width: '200px',
 					borderRadius: '20px'
 				}, 300);
-				//$(this).off('click', resizeUpSearchInput);
 			}
 
 			// resize down text field
@@ -24,12 +24,58 @@ $(function () {
 				$($searchInput).animate({
 					width: '40px'
 				}, 300);
-				//$(this).on('click', resizeUpSearchInput);
+			}
+
+			// get data using wikipedia api
+			function getWikiArticles() {
+				$.ajax({
+					type: 'GET',
+					url: 'https://en.wikipedia.org/w/api.php?callback=?',
+					data: {
+						action: 'query',
+						format: 'json',
+						srsearch: 'Barca',
+						srprop: 'titlesnippet|snippet',
+						list: 'search',
+						sroffset: wikiQueryOffset,
+						continue: ''
+						// prop: 'revisions',
+						// rvprop: 'content',
+						// titles: 'Barca',
+						// prop: 'imageinfo',
+						// iiprop: 'url'
+						// generator: 'allpages',
+						// gaplimit: 6,
+						// gapfilterredir: 'nonredirects',
+					},
+					contentType: "application/json; charset=utf-8",
+					dataType: "json",
+					success: function (data, status) {
+						console.log(data);
+						data.query.search.forEach(function (element) {
+							var link = '<a href="http://en.wikipedia.org/wiki/' + encodeURIComponent(element.title) +'" target="_blank">Read more</a>';
+							$('body').append(element.snippet + link + '<br><br>');
+						});
+						//$('body').append(data.parse.text['*']);
+						wikiQueryOffset += 10;
+					},
+					error: function (error) {
+						console.log(error);
+					}
+				});				
+			}
+
+			// handler for get data from wikipdia
+			function getWikiData(ev) {
+				ev.preventDefault();
+				getWikiArticles();
 			}
 			
 			function bindEvents() {
 				// on click resize input field
 				$searchInput.on('click', resizeUpSearchInput);
+				$($searchInput.closest('form')).on('submit', getWikiData);
+				// resize down search box
 				$('body').on('click', resizeDownSearchInput);
 
 
